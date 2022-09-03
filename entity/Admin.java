bb050e674f47a59ff933b319e1a5bbcd153bb09f
nandan47 <80373995+nandan47@users.noreply.github.com>
2022-09-03 18:17:21 +0530

    Add files via upload

---
+++ entity/Admin.java
@@ -0,0 +1,90 @@
+package net.javaguides.springboot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Admin_Table")
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int devId;
+	private String query;
+	private int feedId;
+	private int respId;
+	private String answer;
+	private boolean status;
+	private boolean statusFeed;
+	private boolean statusResp;
+
+	public boolean isStatusFeed() {
+		return statusFeed;
+	}
+	public void setStatusFeed(boolean statusFeed) {
+		this.statusFeed = statusFeed;
+	}
+	public boolean isStatusResp() {
+		return statusResp;
+	}
+	public void setStatusResp(boolean statusResp) {
+		this.statusResp = statusResp;
+	}
+	public Admin(int devId, String query, int feedId, int respId, String answer, boolean status, boolean statusFeed,
+			boolean statusResp) {
+		super();
+		this.devId = devId;
+		this.query = query;
+		this.feedId = feedId;
+		this.respId = respId;
+		this.answer = answer;
+		this.status = status;
+		this.statusFeed = statusFeed;
+		this.statusResp = statusResp;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	public Admin() {
+		super();
+	}
+	public int getDevId() {
+		return devId;
+	}
+	public void setDevId(int devId) {
+		this.devId = devId;
+	}
+	public String getQuery() {
+		return query;
+	}
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	public int getFeedId() {
+		return feedId;
+	}
+	public void setFeedId(int feedId) {
+		this.feedId = feedId;
+	}
+	public int getRespId() {
+		return respId;
+	}
+	public void setRespId(int respId) {
+		this.respId = respId;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+
+}
