fccca4d68e51ac8d110d31989e1cc30c1df3b966
nandan47 <80373995+nandan47@users.noreply.github.com>
2022-09-03 18:18:15 +0530

    Add files via upload

--- service/AdminService.java
+++ service/AdminService.java
@@ -2,22 +2,22 @@ package net.javaguides.springboot.service;
 
 import org.springframework.http.ResponseEntity;
 
-import net.javaguides.springboot.adminDto.AdminDto;
-import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.Dto.AdminDto;
+import net.javaguides.springboot.entity.Admin;
 
-public interface IUserService {
+public interface AdminService {
 
-	public ResponseEntity<User> blockUser(AdminDto adminDto, int devId);
+	public ResponseEntity<Admin> blockUser(AdminDto adminDto, int devId);
 
-	public ResponseEntity<User> blockFeed(AdminDto adminDto, int feedId);
+	public ResponseEntity<Admin> blockFeed(AdminDto adminDto, int feedId);
 
-	public ResponseEntity<User> blockResp(AdminDto adminDto, int respId);
+	public ResponseEntity<Admin> blockResp(AdminDto adminDto, int respId);
 
-	public ResponseEntity<User> unblockFeed(AdminDto adminDto, int feedId);
+	public ResponseEntity<Admin> unblockFeed(AdminDto adminDto, int feedId);
 
-	public ResponseEntity<User> unblockUser(AdminDto adminDto, int devId);
+	public ResponseEntity<Admin> unblockUser(AdminDto adminDto, int devId);
 
-	public ResponseEntity<User> unblockResp(AdminDto adminDto, int respId);
+	public ResponseEntity<Admin> unblockResp(AdminDto adminDto, int respId);
 
 	
 
--- service/AdminServiceImpl.java
+++ service/AdminServiceImpl.java
@@ -4,33 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import net.javaguides.springboot.adminDto.AdminDto;
-import net.javaguides.springboot.entity.User;
-import net.javaguides.springboot.repository.IUserRepository;
+import net.javaguides.springboot.Dto.AdminDto;
+import net.javaguides.springboot.entity.Admin;
+import net.javaguides.springboot.repository.IAdminRepository;
 
 @Service("IUserService")
-public class IUserServiceImpl implements IUserService{
+public class AdminServiceImpl implements AdminService{
 	
 	@Autowired
-	IUserRepository iUserRepository;
+	IAdminRepository iUserRepository;
 
 	
 	@SuppressWarnings("deprecation")
 	@Override
-	public ResponseEntity<User> blockUser(AdminDto adminDto, int devId) {
+	public ResponseEntity<Admin> blockUser(AdminDto adminDto, int devId) {
 		
-		User getDev =iUserRepository.getById(devId);
+		Admin getDev =iUserRepository.getById(devId);
 		getDev.setStatus(false);
-		User updateDev =iUserRepository.save(getDev);
+		Admin updateDev =iUserRepository.save(getDev);
 		return ResponseEntity.ok().body(updateDev);
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
-	public ResponseEntity<User> blockFeed(AdminDto adminDto, int feedId) {
-		User getDev=iUserRepository.getById(feedId);
+	public ResponseEntity<Admin> blockFeed(AdminDto adminDto, int feedId) {
+		Admin getDev=iUserRepository.getById(feedId);
 		getDev.setStatusFeed(false);
-		User updateDev =iUserRepository.save(getDev);
+		Admin updateDev =iUserRepository.save(getDev);
 		
 		return ResponseEntity.ok().body(updateDev);
 	}
@@ -39,42 +39,42 @@ public class IUserServiceImpl implements IUserService{
 	
 	@SuppressWarnings("deprecation")
 	@Override
-	public ResponseEntity<User> blockResp(AdminDto adminDto, int respId) {
-		User getDev=iUserRepository.getById(respId);
+	public ResponseEntity<Admin> blockResp(AdminDto adminDto, int respId) {
+		Admin getDev=iUserRepository.getById(respId);
 		getDev.setStatusResp(false);
-		User updateDev =iUserRepository.save(getDev);
+		Admin updateDev =iUserRepository.save(getDev);
 		
 		return ResponseEntity.ok().body(updateDev);
 		
 	}
 
 	@Override
-	public ResponseEntity<User> unblockFeed(AdminDto adminDto, int feedId) {
+	public ResponseEntity<Admin> unblockFeed(AdminDto adminDto, int feedId) {
 		@SuppressWarnings("deprecation")
-		User getDev=iUserRepository.getById(feedId);
+		Admin getDev=iUserRepository.getById(feedId);
 		getDev.setStatusFeed(true);
-		User updateDev =iUserRepository.save(getDev);
+		Admin updateDev =iUserRepository.save(getDev);
 		
 		return ResponseEntity.ok().body(updateDev);
 		
 	}
 
 	@Override
-	public ResponseEntity<User> unblockUser(AdminDto adminDto, int devId) {
+	public ResponseEntity<Admin> unblockUser(AdminDto adminDto, int devId) {
 		
 		@SuppressWarnings("deprecation")
-		User getDev =iUserRepository.getById(devId);
+		Admin getDev =iUserRepository.getById(devId);
 		getDev.setStatus(true);
-		User updateDev =iUserRepository.save(getDev);
+		Admin updateDev =iUserRepository.save(getDev);
 		return ResponseEntity.ok().body(updateDev);
 	}
 
 	@Override
-	public ResponseEntity<User> unblockResp(AdminDto adminDto, int respId) {
+	public ResponseEntity<Admin> unblockResp(AdminDto adminDto, int respId) {
 		@SuppressWarnings("deprecation")
-		User getDev=iUserRepository.getById(respId);
+		Admin getDev=iUserRepository.getById(respId);
 		getDev.setStatusResp(true);
-		User updateDev =iUserRepository.save(getDev);
+		Admin updateDev =iUserRepository.save(getDev);
 		
 		return ResponseEntity.ok().body(updateDev);
 	}
