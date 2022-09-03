0696f261e1b994f181bb790487bac2005f167a9c
SuyogNimsatkar <91984632+SuyogNimsatkar@users.noreply.github.com>
2022-09-03 16:17:24 +0530

    Add files via upload

--- controller/UserController.java
+++ controller/UserController.java
@@ -1,67 +1,67 @@
-package net.javaguides.springboot.controller;
+package com.devcom.controller;
+
+import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.javaguides.springboot.adminDto.AdminDto;
-import net.javaguides.springboot.entity.User;
-import net.javaguides.springboot.service.IUserService;
+import com.devcom.dto.UserDto;
+import com.devcom.entity.User;
+import com.devcom.repository.UserRepo;
+import com.devcom.service.UserIService;
 
 @RestController
-@RequestMapping("/devapp-admin")
+//@RequestMapping("/register")
 public class UserController {
 	
+	@Autowired
+	UserIService userService;
 	
 	@Autowired
-	IUserService iUserService;
+	UserRepo userRepo;
 	
-	@PutMapping("/BlockFeed/{feedId}")
-	public ResponseEntity<User> blockFeed(@PathVariable("feedId") int feedId,@RequestBody AdminDto adminDto){
-		return this.iUserService.blockFeed(adminDto,feedId);
-	}
-	
-	@PutMapping("/UnblockFeed/{feedId}")
-	public ResponseEntity<User> unblockFeed(@PathVariable("feedId") int feedId,@RequestBody AdminDto adminDto){
-		return this.iUserService.unblockFeed(adminDto,feedId);
-	}
-	
-	@PutMapping("/BlockUser/{devId}")
-	public ResponseEntity<User> blockUser(@PathVariable("devId") int devId,@RequestBody AdminDto adminDto){
-		return this.iUserService.blockUser(adminDto,devId);
-	}
-	
-	@PutMapping("/UnBlockUser/{devId}")
-	public ResponseEntity<User> unblockUser(@PathVariable("devId") int devId,@RequestBody AdminDto adminDto){
-		return this.iUserService.unblockUser(adminDto,devId);
+	@PostMapping("/register")
+	public ResponseEntity<String> registerUser(@RequestBody UserDto userdto) {
+		return userService.registerUser(userdto);
+			
 	}
 	
 	
-	@PutMapping("/BlockResp/{respId}")
-	public ResponseEntity<User> blockResp(@PathVariable("respId") int respId,@RequestBody AdminDto adminDto){
-		return this.iUserService.blockResp(adminDto,respId);
+//	@PostMapping
+//	public String loginUser(@RequestBody Map<String, String> userdto) {
+//		String username = userdto.getOrDefault("username", null);
+//		String password = userdto.getOrDefault("password", null);
+//		Optional<User> opt1 = userRepo.findByUserName(username);
+//		System.out.println(opt1.get().getPassword());
+//		if(opt1.isPresent() && opt1.get().getPassword().equals(password)) {
+//			return "User Loin Success";
+//		}else {
+//			return "Incorrect credentials";
+//		}
+//		
+//	}
+	
+	
+	@PostMapping("/login")
+	public ResponseEntity<String> loginUser(@ModelAttribute UserDto userdto) {
+		String username = userdto.getUserName();
+		String password = userdto.getPassword();
+		Optional<User> opt = userRepo.findByUserName(username);
+		
+		if(opt.isPresent() && opt.get().getPassword().equals(password)) {
+			return new ResponseEntity<String>("Login successfully", HttpStatus.OK);
+		}else {
+			return new ResponseEntity<String>("Incorrect credentials", HttpStatus.OK);
+		}
 	}
-	
-	@PutMapping("/UnBlockResp/{respId}")
-	public ResponseEntity<User> unblockResp(@PathVariable("respId") int respId,@RequestBody AdminDto adminDto){
-		return this.iUserService.unblockResp(adminDto,respId);
-	}
-
-	
-	
-	
-	
-	
-	
-	
 	
 
-	
-	
 }
